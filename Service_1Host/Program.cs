a5c24325173b50c71a760bf5087905df824550cc
Nermienkh <nermien.khalifa@gmail.com>
2022-05-22 14:24:15 +0200

    Service_1Host reloaded to the solution

---
+++ Service_1Host/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Service_1Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (ServiceHost  host= new ServiceHost(typeof(WcfServiceLibrary_1.Service1))) 
+            {
+                host.Open();
+                Console.WriteLine("host started at "+DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
